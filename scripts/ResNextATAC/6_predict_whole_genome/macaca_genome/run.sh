989c6a9e91533552c5ba429b337e6a271be03a63
ehcilc23 <150108916+ehcilc23@users.noreply.github.com>
2025-07-25 15:49:07 +0800

    Update

--- scripts/ResNextATAC/6_predict_whole_genome/macaca_genome/run.sh
+++ scripts/ResNextATAC/6_predict_whole_genome/macaca_genome/run.sh
@@ -7,25 +7,31 @@
 #SBATCH --output=%J.out
 #SBATCH --error=%J.err
 
-
 python pred_Macaca_chr.py --region Region.*.slide.Macaca.input.txt --anno Female_mus_anno_5w.txt
-ct=`tail -n+2 ./Female_mus_anno_5w.txt |cut -f 5|sort|uniq`
-for i in $ct;do
-cat Predict_track/test_predict_"$i"_*.txt >Predict_track/merged_"$i".txt;
-done
 
-#get bedgraph
+species_chrom_size=Mfascicularis_6.0.chrom.sizes
+
+#ct=`tail -n+2 ./Female_mus_anno_5w.txt |cut -f 5|sort|uniq`
+#for i in $ct;do
+#cat Predict_track/test_predict_"$i"_*.txt >Predict_track/merged_"$i".txt;
+#done
+
+# get bedgraph
 for x in `ls ./Predict_track/merged_*`; do 
 cat $x | perl -ne '@a=($_ =~ /(.*):(\d+)-(\d+)\t(.*)/); print "".join("\t", $a[0],  $a[1]+250-50, $a[1]+250+50, $a[3]), "\n";' > `echo ${x}|sed 's/\.txt//g'`.bedGraph;
 done
 
-#get bw
+# get bw
+cat ${species_chrom_size} |awk '{print $1"\t"0"\t"$2}' >chrom.size
+
 for x in `ls ./Predict_track/merged_*.bedGraph`; do 
-tmp=`echo ${x} | sed 's#merged_##g' `
-sort -k1,1V -k2,2n -k3,3n $x  >tmp
-bedClip tmp chrom.size $tmp
-tmp1=`echo ${tmp} | sed 's#.bedGraph##g' `
-bedGraphToBigWig $tmp chrom.size ${tmp1}.bw; 
+subanno_bedgraph=`echo ${x} | sed 's#merged_##g' `
+subanno_id=`echo ${subanno_bedgraph} | sed 's#.bedGraph##g' `
+echo $subanno_id
+bedtools intersect -a $x -b chrom.size >tmp
+sort -k1,1V -k2,2n -k3,3n tmp >$subanno_bedgraph 
+bedGraphToBigWig $subanno_bedgraph ${species_chrom_size} ${subanno_id}.bw
+rm tmp;
 done
 
 mkdir bw
